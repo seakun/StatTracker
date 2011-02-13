d5044e982003bd52427247fbc88b125721179c54
chris.lebowitz@gmail.com <chris.lebowitz@gmail.com@17ca8119-205c-cd62-ded4-31256c700473>
2011-02-13 21:16:55 +0000

    git-svn-id: https://stattracker.googlecode.com/svn/trunk@29 17ca8119-205c-cd62-ded4-31256c700473

--- StatTracker/app/models/batting_stat.rb
+++ StatTracker/app/models/batting_stat.rb
@@ -1,5 +1,5 @@
 class BattingStat < ActiveRecord::Base
-    attr_accessible :player_id, :team_id, :stint, :games, :games_batting, :plate_appearances, :at_bats, :runs, :hits, :doubles, :triples, :home_runs, :total_bases, :extra_base_hits, :rbi, :stolen_bases, :caught_stealing, :walks, :strikeouts, :intentional_walks, :hit_by_pitch, :sacrifice_hits, :sacrifice_flies, :grounded_into_double_plays
+  attr_accessible :player_id, :team_id, :stint, :games, :games_batting, :plate_appearances, :at_bats, :runs, :hits, :doubles, :triples, :home_runs, :total_bases, :extra_base_hits, :rbi, :stolen_bases, :caught_stealing, :walks, :strikeouts, :intentional_walks, :hit_by_pitch, :sacrifice_hits, :sacrifice_flies, :grounded_into_double_plays
 
 	belongs_to :player
 	belongs_to :team
