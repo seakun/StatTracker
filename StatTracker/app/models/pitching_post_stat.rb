218392f3dba5b9d855161a72827eb0ddcbfa8bfd
chris.lebowitz@gmail.com <chris.lebowitz@gmail.com@17ca8119-205c-cd62-ded4-31256c700473>
2011-02-24 21:07:28 +0000

    git-svn-id: https://stattracker.googlecode.com/svn/trunk@56 17ca8119-205c-cd62-ded4-31256c700473

--- StatTracker/app/models/pitching_post_stat.rb
+++ StatTracker/app/models/pitching_post_stat.rb
@@ -5,13 +5,15 @@ class PitchingPostStat < ActiveRecord::Base
 	belongs_to :team
 	
 	def self.single_season_sort(stat)
-		PitchingPostStat.find(:all, :select => [:player_id, :team_id, stat.to_sym], :order => stat + " DESC", :limit => 50)
+    s = accessible_attributes.include?(stat)? stat.to_s : send("str_" + stat)
+    min_ip = accessible_attributes.include?(stat)? 0 : 300
+		PitchingStat.find(:all, :conditions => ["innings_pitched_outs > ?", min_ip], :order => s + " DESC", :limit => 50)
 	end
-	
+
 	def self.career_sort(stat)
-		stats = PitchingPostStat.find(:all, :select => [:player_id, stat.to_sym], :joins => [:player])
+		stats = PitchingStat.find(:all, :select => [:player_id, stat.to_sym], :joins => [:player])
 		comb = {}
-		stats.each { |s| 
+		stats.each { |s|
 			if comb.has_key?(s.player_id)
 					comb[s.player_id] += s.send(stat).to_i
 			else
@@ -21,18 +23,18 @@ class PitchingPostStat < ActiveRecord::Base
 					comb.store(s.player_id, s.send(stat))
 				end
 			end
-		} 
+		}
 		sorted = comb.sort{|a,b| b[1] <=> a[1]}
-		sorted.take(50).each { |a| 
+		sorted.take(50).each { |a|
 		a[0] = Player.find(a[0])
 		}
 		return sorted.take(50)
 	end
-	
+
 	def self.active_sort(stat)
-		stats = PitchingPostStat.find(:all, :select => [:player_id, stat.to_sym], :conditions => ["final_game is NULL"], :joins => [:player])
+		stats = PitchingStat.find(:all, :select => [:player_id, stat.to_sym], :conditions => ["final_game is NULL"], :joins => [:player])
 		comb = {}
-		stats.each { |s| 
+		stats.each { |s|
 			if comb.has_key?(s.player_id)
 					comb[s.player_id] += s.send(stat).to_i
 			else
@@ -42,9 +44,9 @@ class PitchingPostStat < ActiveRecord::Base
 					comb.store(s.player_id, s.send(stat))
 				end
 			end
-		} 
+		}
 		sorted = comb.sort{|a,b| b[1] <=> a[1]}
-		sorted.take(50).each { |a| 
+		sorted.take(50).each { |a|
 		a[0] = Player.find(a[0])
 		}
 		return sorted.take(50)
@@ -55,52 +57,98 @@ class PitchingPostStat < ActiveRecord::Base
 	end
 
 	def win_loss_percentage()
-		sprintf("%.3f", (wins / (wins + losses)))
+		sprintf("%.3f", (wins / (wins + losses).to_f))
 	end
 
 	def innings_pitched
-		innings_pitched_outs / 3
+		sprintf("%.1f", innings_pitched_outs / 3.to_f)
 	end
 
 	def era
-		(earned_runs * 9) / inings_pitched
+		sprintf("%.2f", (earned_runs * 9) / innings_pitched.to_f)
 	end
 
 	def opponents_batting_average
-		(hits) / (batters_faced - walks - hit_by_pitch - intentional_walks)
+		(hits) / (batters_faced - walks - hit_by_pitch - intentional_walks).to_f
 	end
 
 	def walks_and_hits_innings_pitched
-		(walks + hits) / innings_pitched
+		(walks + hits) / innings_pitched.to_f
 	end
 
 	def hits_innings
-		(hits * 9) / innings_pitched
+		(hits * 9) / innings_pitched.to_f
 	end
 
 	def home_runs_innings
-		(home_runs * 9) / innings_pitched
+		(home_runs * 9) / innings_pitched.to_f
 	end
 
 	def walks_innings
-		(walks * 9) / innings_pitched
+		(walks * 9) / innings_pitched.to_f
 	end
 
 	def strikeouts_innings
-		(strikeouts * 9) / innings_pitched
+		(strikeouts * 9) / innings_pitched.to_f
 	end
 
 	def strikeouts_walks
-		(strikeouts / walks)
+		(strikeouts / walks.to_f)
 	end
 
 	def adjusted_era
-		league_era = Pitchingstat.where(year => self.year).average(era)
+		league_era = PitchingStat.where(year => self.year).average(era)
 		100 * (league_era / era)
 	end
 
-	def base_runs
-	
+  private
+
+  def self.multiplier
+    10000
+  end
+
+  def self.str_win_loss_percentage()
+		"((wins * #{multiplier}) / (wins + losses))"
 	end
-	
+
+	def self.str_innings_pitched
+		"innings_pitched_outs * #{multiplier} / 3"
+	end
+
+	def self.str_era
+		"((earned_runs * 9) * #{multiplier}) / (innings_pitched_outs / 3)"
+	end
+
+	def self.str_opponents_batting_average
+		"(hits * #{multiplier}) / (batters_faced - walks - hit_by_pitch - intentional_walks)"
+	end
+
+	def self.str_walks_and_hits_innings_pitched
+		"(walks + hits) * #{multiplier} / (innings_pitched_outs / 3)"
+	end
+
+	def self.str_hits_innings
+		"(hits * 9) * #{multiplier} / (innings_pitched_outs / 3)"
+	end
+
+	def self.str_home_runs_innings
+		"(home_runs * 9) * #{multiplier} / (innings_pitched_outs / 3)"
+	end
+
+	def self.str_walks_innings
+		"(walks * 9) * #{multiplier} / (innings_pitched_outs / 3)"
+	end
+
+	def self.str_strikeouts_innings
+		"(strikeouts * 9) * #{multiplier} / (innings_pitched_outs / 3)"
+	end
+
+	def self.str_strikeouts_walks
+		"(strikeouts * #{multiplier} / walks)"
+	end
+
+	def self.str_adjusted_era
+		league_era = PitchingStat.where(year => self.year).average(era)
+		"100 * #{multiplier} * (#{league_era} / era)"
+	end
 end
--- StatTracker/app/models/pitching_stat.rb
+++ StatTracker/app/models/pitching_stat.rb
@@ -5,7 +5,9 @@ class PitchingStat < ActiveRecord::Base
 	belongs_to :team
 	
 	def self.single_season_sort(stat)
-		PitchingStat.find(:all, :select => [:player_id, :team_id, stat.to_sym], :order => stat + " DESC", :limit => 50)
+    s = accessible_attributes.include?(stat)? stat.to_s : send("str_" + stat)
+    min_ip = accessible_attributes.include?(stat)? 0 : 300
+		PitchingStat.find(:all, :conditions => ["innings_pitched_outs > ?", min_ip], :order => s + " DESC", :limit => 50)
 	end
 
 	def self.career_sort(stat)
@@ -55,52 +57,98 @@ class PitchingStat < ActiveRecord::Base
 	end
 
 	def win_loss_percentage()
-		sprintf("%.3f", (wins / (wins + losses)))
+		sprintf("%.3f", (wins / (wins + losses).to_f))
 	end
 
 	def innings_pitched
-		innings_pitched_outs / 3
+		sprintf("%.1f", innings_pitched_outs / 3.to_f)
 	end
 
 	def era
-		(earned_runs * 9) / inings_pitched
+		sprintf("%.2f", (earned_runs * 9) / innings_pitched.to_f)
 	end
 
 	def opponents_batting_average
-		(hits) / (batters_faced - walks - hit_by_pitch - intentional_walks)
+		(hits) / (batters_faced - walks - hit_by_pitch - intentional_walks).to_f
 	end
 
 	def walks_and_hits_innings_pitched
-		(walks + hits) / innings_pitched
+		(walks + hits) / innings_pitched.to_f
 	end
 
 	def hits_innings
-		(hits * 9) / innings_pitched
+		(hits * 9) / innings_pitched.to_f
 	end
 
 	def home_runs_innings
-		(home_runs * 9) / innings_pitched
+		(home_runs * 9) / innings_pitched.to_f
 	end
   
 	def walks_innings
-		(walks * 9) / innings_pitched
+		(walks * 9) / innings_pitched.to_f
 	end
 
 	def strikeouts_innings
-		(strikeouts * 9) / innings_pitched
+		(strikeouts * 9) / innings_pitched.to_f
 	end
 
 	def strikeouts_walks
-		(strikeouts / walks)
+		(strikeouts / walks.to_f)
 	end
 
 	def adjusted_era
-		league_era = Pitchingstat.where(year => self.year).average(era)
+		league_era = PitchingStat.where(year => self.year).average(era)
 		100 * (league_era / era)
 	end
 
-	def base_runs
+  private
 
+  def self.multiplier
+    10000
+  end
+
+  def self.str_win_loss_percentage()
+		"((wins * #{multiplier}) / (wins + losses))"
 	end
 
+	def self.str_innings_pitched
+		"innings_pitched_outs * #{multiplier} / 3"
+	end
+
+	def self.str_era
+		"((earned_runs * 9) * #{multiplier}) / (innings_pitched_outs / 3)"
+	end
+
+	def self.str_opponents_batting_average
+		"(hits * #{multiplier}) / (batters_faced - walks - hit_by_pitch - intentional_walks)"
+	end
+
+	def self.str_walks_and_hits_innings_pitched
+		"(walks + hits) * #{multiplier} / (innings_pitched_outs / 3)"
+	end
+
+	def self.str_hits_innings
+		"(hits * 9) * #{multiplier} / (innings_pitched_outs / 3)"
+	end
+
+	def self.str_home_runs_innings
+		"(home_runs * 9) * #{multiplier} / (innings_pitched_outs / 3)"
+	end
+
+	def self.str_walks_innings
+		"(walks * 9) * #{multiplier} / (innings_pitched_outs / 3)"
+	end
+
+	def self.str_strikeouts_innings
+		"(strikeouts * 9) * #{multiplier} / (innings_pitched_outs / 3)"
+	end
+
+	def self.str_strikeouts_walks
+		"(strikeouts * #{multiplier} / walks)"
+	end
+
+	def self.str_adjusted_era
+		league_era = PitchingStat.where(year => self.year).average(era)
+		"100 * #{multiplier} * (#{league_era} / era)"
+	end
 end
