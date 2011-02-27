cf17a1f110ccb720fef79ecf39ee1e31d657ac87
iamtheseankung@gmail.com <iamtheseankung@gmail.com@17ca8119-205c-cd62-ded4-31256c700473>
2011-02-27 06:36:53 +0000

    ~added a home page view ~started working on players UI
    git-svn-id: https://stattracker.googlecode.com/svn/trunk@63 17ca8119-205c-cd62-ded4-31256c700473

--- StatTracker/config/routes.rb
+++ StatTracker/config/routes.rb
@@ -1,5 +1,7 @@
 StatTracker::Application.routes.draw do
-  root :to => "home#index"
+  
+  
+  match 'home/index' => 'home#index', :as => 'home#index'
 
   devise_for :users
   
@@ -105,7 +107,7 @@ StatTracker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
