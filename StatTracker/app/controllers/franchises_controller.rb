fdffa04d3ce36aead6d6dbabb62bc8e0ae0d0cd8
czhaocmu <czhaocmu@17ca8119-205c-cd62-ded4-31256c700473>
2011-03-27 19:34:46 +0000

    git-svn-id: https://stattracker.googlecode.com/svn/trunk@122 17ca8119-205c-cd62-ded4-31256c700473

--- StatTracker/app/controllers/franchises_controller.rb
+++ StatTracker/app/controllers/franchises_controller.rb
@@ -5,14 +5,29 @@ class FranchisesController < ApplicationController
 
   def show
     @franchise = Franchise.find(params[:id])
+    @google_image = GoogleImage.all(@franchise.name+" logo", 0).first
     @teams= Team.find(:all, :conditions => ['franchise_id = ?', params[:id]])
     @chart = GoogleVisualr::Table.new
-		@chart.add_column('number' , 'Year')
 		@chart.add_column('string' , 'Team')
+    @chart.add_column('number' , 'Year')
 		@chart.add_column('string' , 'League')
 		@chart.add_column('number' , 'Wins')
 		@chart.add_column('number' , 'Losses')
-    @chart.add_column('number' , 'AB')
+    @chart.add_column('number' , 'Rank')
+    @chart.add_rows(@teams.size)
+    @teams.each { |b|
+			i = @teams.index(b)
+			@chart.set_cell(i, 0, b.name)
+			@chart.set_cell(i, 1, b.year)
+      @chart.set_cell(i, 2, b.division.league.name)
+      @chart.set_cell(i, 3, b.wins)
+      @chart.set_cell(i, 4, b.losses)
+      @chart.set_cell(i, 5, b.rank)
+    }
+    options = { :width => 600 }
+    options.each_pair do | key, value |
+    @chart.send "#{key}=", value
+  end
   end
 
   def new
