9438c52fd17623fceb7cc50df82b8e02382be14a
acaniglia614 <acaniglia614@17ca8119-205c-cd62-ded4-31256c700473>
2011-04-23 23:11:49 +0000

    git-svn-id: https://stattracker.googlecode.com/svn/trunk@262 17ca8119-205c-cd62-ded4-31256c700473

--- StatTracker/app/controllers/franchises_controller.rb
+++ StatTracker/app/controllers/franchises_controller.rb
@@ -18,20 +18,20 @@ autocomplete :franchise, :name, :full => true
 		@chart.add_column('string' , 'Team')
     @chart.add_column('number' , 'Year')
 		@chart.add_column('string' , 'League')
-		@chart.add_column('number' , 'Wins')
-		@chart.add_column('number' , 'Losses')
+		@chart.add_column('string' , 'Wins')
+		@chart.add_column('string' , 'Losses')
     @chart.add_column('string' , 'W-L%')
-    @chart.add_column('number' , 'Rank')
+    @chart.add_column('string' , 'Rank')
     @chart.add_rows(@teams.size)
     @teams.each { |b|
 			i = @teams.index(b)
 			@chart.set_cell(i, 0, "<a href='/teams/#{b.id}'>#{b.name}</a>")
 			@chart.set_cell(i, 1, b.year)
       @chart.set_cell(i, 2, b.division.league.name)
-      @chart.set_cell(i, 3, b.wins)
-      @chart.set_cell(i, 4, b.losses)
+      @chart.set_cell(i, 3, b.wins.to_s)
+      @chart.set_cell(i, 4, b.losses.to_s)
       @chart.set_cell(i, 5, b.pct.to_s)
-      @chart.set_cell(i, 6, b.rank)
+      @chart.set_cell(i, 6, b.rank.to_s)
     }
     options = { :width => 600, :allowHtml => true }
     options.each_pair do | key, value |
